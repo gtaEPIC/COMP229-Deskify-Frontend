f2b16fff775cc6907aa07b4cae8d2c25a521d223
gtaEPIC <johnnygotrain@gmail.com>
2023-11-25 14:05:38 -0500

    Added routes and Navigation bar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './Header';
+import Home from './pages/Home';
+import Tickets from "./pages/Tickets";
+import NewTickets from "./pages/NewTickets";
+import Login from "./pages/Login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/*"
+                    element={
+                        <>
+                            <Header />
+                            <br />
+                            <Routes>
+                                <Route index element={<Home />} />
+                                <Route path="/tickets/new" element={<NewTickets />} />
+                                <Route path="/tickets/" element={<Tickets />} />
+                                <Route path="/login" element={<Login />} />"}
+                                <Route path="*" element={<App />} />
+                            </Routes>
+                        </>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
