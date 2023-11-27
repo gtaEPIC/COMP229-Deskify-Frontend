d7ae533dbf78c79072bea3b2e60d94294204c6b4
gtaEPIC <johnnygotrain@gmail.com>
2023-11-27 14:29:38 -0500

    Fixed the painfully large gaps of line breaks
    Makes me suspect that chatGPT was involved in this

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -20,20 +20,15 @@ function Tickets() {
                 console.error('Error:', error);
             }
         };
-
         fetchTickets();
     }, []);
 
   return (
     <div>
-     
      <center><h2>Tickets</h2></center> 
      <Link to="/tickets/new" className="btn btn-primary">
         Create new tickets
       </Link>
-     
-     
-     
       <br></br>
     <br></br>
      <table className="table table-dark">
@@ -46,9 +41,6 @@ function Tickets() {
       <th scope="col">Created</th>
       <th scope="col">Last Update</th>
       <th scope="col">View</th>
-
-
-
     </tr>
   </thead>
   <tbody>
