916b593b89cb1a2a7599dcd7d1192bb408e75fd9
imirza7 <127257130+imirza7@users.noreply.github.com>
2023-12-06 23:58:08 -0500

    Visual Changes to the Header and Main.js

--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,30 @@
 import {Link, NavLink, useLocation} from "react-router-dom";
 import logo from "./images/logo.png";
 import {getUsername, isAuthenticated, logout} from "./pages/login-helper";
+import'./header.css';
 
 function Header() {
     // eslint-disable-next-line no-unused-vars
     const loc = useLocation();
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "rgb(72, 150, 160)", padding: 0, height: '64px' }}>
+            <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "#08181C", padding: 0, height: '64px' }}>
                     <NavLink to="#" className="navbar-brand" style={{ marginLeft: '0px' }}>
                         <img src={logo} alt="logo" width="64" height="64" />
                     </NavLink>
                 <div className="container-fluid">
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
-                            <li className="nav-item"><NavLink to="/" className="nav-link" style={{ color: "white"}}>Home</NavLink></li>
-                            <li className="nav-item dropdown">
-                                <Link className="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" to="#" style={{ color: "white"}}>
+                            <button><li className="nav-item"><NavLink to="/" className="nav-link" style={{ color: "#b3c1b4"}}>Home</NavLink></li></button>
+                            <button><li className="nav-item dropdown">
+                                <Link className="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" to="#" style={{ color: "#b3c1b4"}}>
                                     Tickets
                                 </Link>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><NavLink to="/tickets" className="dropdown-item">List of tickets</NavLink></li>
                                     {isAuthenticated() && (<li><NavLink to="/tickets/new" className="dropdown-item">New Ticket</NavLink></li>)}
                                 </ul>
-                            </li>
+                            </li></button>
                             {isAuthenticated() && (
                                 <li className="nav-item dropdown">
                                     <Link className="nav-link dropdown-toggle" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" to="#">
@@ -35,7 +36,10 @@ function Header() {
                                 </li>
                             )}
                             {!isAuthenticated() && (
-                                <li className="nav-item"><NavLink to="/login" className="nav-link" style={{ color: "white"}}>Login</NavLink></li>
+                                <button><li className="nav-item"><NavLink to="/login" className="nav-link" style={{ color: "#b3c1b4"}}>Login</NavLink></li></button>
+                            )}
+                            {!isAuthenticated() && (
+                                <button><li className="nav-item"><NavLink to="" className="nav-link" style={{ color: "#b3c1b4"}}>Sign-up</NavLink></li></button>
                             )}
                         </ul>
                     </div>
