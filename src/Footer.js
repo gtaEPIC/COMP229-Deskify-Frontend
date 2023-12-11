015f351af8ecf5bf3873458a8b64628bc49a7193
mohammed subhan asif hakeem <145624099+Subhan636@users.noreply.github.com>
2023-12-11 04:29:18 -0500

    changes to Web App

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+// src/Footer.js
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>Deskify - Help Desk</p>
+                <p>© 2023 All rights reserved</p>
+                <p>Contact us: support@deskify.com</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
