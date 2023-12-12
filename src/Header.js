89cc1a2d32b22152235e27bb5dcd694562338035
gtaEPIC <johnnygotrain@gmail.com>
2023-12-12 14:36:25 -0500

    Fixed the navigation bar

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,19 @@
 // Header.js
 
 import React, { useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import {NavLink, Link, useLocation} from 'react-router-dom';
 import logo from './images/logo.png';
 import { getUsername, isAuthenticated, logout, getIsAdmin } from './pages/login-helper';
 import './header.css';
 
 function Header() {
-  const [showUserProfile, setShowUserProfile] = useState(false);
+  // Redundant...
+  // const [showUserProfile, setShowUserProfile] = useState(false);
+  // const handleUserProfileClick = () => {
+  //   setShowUserProfile(!showUserProfile);
+  // };
 
-  const handleUserProfileClick = () => {
-    setShowUserProfile(!showUserProfile);
-  };
+  const loc = useLocation();
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: '#08181C', padding: 0, height: '64px' }}>
@@ -20,58 +22,58 @@ function Header() {
       </NavLink>
       <div className="container-fluid">
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link text-secondary">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <Link to="/tickets" className="nav-link text-secondary">
-                Tickets
-              </Link>
-            </li>
-            {getIsAdmin() && (
+          <ul className="navbar-nav ms-auto me-5">
+            <button>
               <li className="nav-item">
-                <NavLink to="/Profile" className="nav-link text-secondary">
-                  Profile
+                <NavLink to="/" className="nav-link text-secondary">
+                  Home
                 </NavLink>
               </li>
-            )}
+            </button>
+            <button>
+              <li className="nav-item dropdown">
+                <Link className="nav-link dropdown-toggle text-secondary" id="navbarDropdown" data-bs-toggle="dropdown"
+                      aria-expanded="false" to="#">
+                  Tickets
+                </Link>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><NavLink to="/tickets" className="dropdown-item">List of tickets</NavLink></li>
+                  {isAuthenticated() && (
+                      <li><NavLink to="/tickets/new" className="dropdown-item">New Ticket</NavLink></li>)}
+                </ul>
+              </li>
+            </button>
             {isAuthenticated() ? (
-              <>
-                <li className="nav-item dropdown">
-                  <Link to="#" className="nav-link dropdown-toggle text-secondary" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false" onClick={handleUserProfileClick}>
+                <>
+                  <li className="nav-item dropdown me-2">
+                    <Link className="nav-link dropdown-toggle text-secondary" id="navbarDropdown"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false" to="#">
                     {getUsername()}
-                  </Link>
-                  {showUserProfile && (
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <Link to="/" onClick={logout} className="dropdown-item">
-                        Logout
-                      </Link>
-                    </div>
-                  )}
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/profile" className="nav-link text-secondary">
-                    <i className="fas fa-user" style={{ marginRight: '5px' }}></i>
-                    Profile
-                  </NavLink>
-                </li>
-              </>
+                    </Link>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><NavLink to="/profile" className="dropdown-item">Edit Profile</NavLink></li>
+                      <li><Link to="/" className="dropdown-item" onClick={logout}>Logout</Link></li>
+                    </ul>
+                  </li>
+                </>
             ) : (
-              <>
-                <li className="nav-item">
-                  <NavLink to="/login" className="nav-link text-secondary">
-                    Login
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/signup" className="nav-link text-secondary">
-                    Sign-up
-                  </NavLink>
-                </li>
-              </>
+                <>
+                  <button>
+                    <li className="nav-item">
+                      <NavLink to="/login" className="nav-link text-secondary">
+                        Login
+                      </NavLink>
+                    </li>
+                  </button>
+                  <button>
+                    <li className="nav-item">
+                      <NavLink to="/signup" className="nav-link text-secondary">
+                        Sign-up
+                      </NavLink>
+                    </li>
+                  </button>
+                </>
             )}
           </ul>
         </div>
@@ -80,4 +82,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default Header;
